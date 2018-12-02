ff058969055513c76fd4a6a86a4db8508e737e55
Johnny McMahan <jm0189055@otc.edu>
2018-12-02 13:18:07 -0600

    twitter config

---
+++ config.js
@@ -0,0 +1,7 @@
+//config.js
+module.exports = {
+  consumer_key: 'ORbWcVq7pJfEXTI5x6WcdWvJD',
+  consumer_secret: 'xnwKgMto0pdoyhV6a2PRKwZJpGDQtmDog5GMtNHKPK2upymfW3',
+  access_token: '985551010394501120-iBlPbowtU9SFxWxzxEH8g2OyoAzcIB6',
+  access_token_secret: 'gBFmJJajcy6dlJsFVYPluldxHvg3EMCXcS8rjgwijkP2q'
+}
